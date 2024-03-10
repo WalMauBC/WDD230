2071f3cb146449004fab3c39e772c39eb5fcd4cf
Waldo Mauricio BC <112277562+WalMauBC@users.noreply.github.com>
2024-03-09 20:46:43 -0500

    W01

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,22 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    var footer = document.querySelector("footer");
+document.getElementById('year').textContent = new Date().getFullYear();
 
-    
-    var copyrightYearParagraph = footer.querySelector("p:first-child");
-
-    
-    var lastModifiedDateParagraph = footer.querySelector("p:nth-child(2)");
-
-    
-    var currentYear = new Date().getFullYear();
-
-   
-    copyrightYearParagraph.textContent = "Copyright © " + currentYear + " Brigham Young University-Idaho | All rights reserved";
-
-   
-    var lastModifiedDate = new Date(document.lastModified);
-
-    
-    lastModifiedDateParagraph.textContent = "Last modified: " + lastModifiedDate.toLocaleString();
-});
+document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
